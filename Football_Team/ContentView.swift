75d94395879d5ffc436b91ea97a4db785b513948
abdiysf <mustafeabdisalam@gmail.com>
2024-11-16 22:41:08 +0300

    Initial Commit

---
+++ Football_Team/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football_Team
+//
+//  Created by Abdirahman Standard on 16/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football_Team/Football_TeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_TeamApp.swift
+//  Football_Team
+//
+//  Created by Abdirahman Standard on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Football_TeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
